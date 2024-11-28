f225a1e6724841b5a2be062bbd6d0b944566dc33
Ruben de Bruin <34062862+RubendeBruin@users.noreply.github.com>
2024-11-28 13:42:02 +0100

    Added optional packages

--- run.py
+++ run.py
@@ -1,7 +1,24 @@
 import numpy
 import scipy
 import xarray
-import netCDF4
+import numpy
+import scipy
+import xarray
 import vtk
+import PySide6
+import pyqtgraph
+import requests
+import netCDF4
+import fpdf  # from fpdf2
+import pdfrw
+import rapidfuzz
+import wavespectra
+import waveresponse
+import mafredo
+import nooverlap
+import PySide6QtAds
+import PIL
+from yaml import load, safe_dump, SafeLoader
+
 
 print('all imported!')
