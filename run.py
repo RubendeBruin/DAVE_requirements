f2865de0b3b4bb0b863ff08ef0f9a31e94d9f96c
Ruben de Bruin <34062862+RubendeBruin@users.noreply.github.com>
2024-11-28 15:13:43 +0100

    main deps

--- run.py
+++ run.py
@@ -2,18 +2,18 @@ from numpy import *
 from scipy import *
 from numpy import *
 from scipy import *
-from xarray import *
+# from xarray import *
 from vtk import *
 from PySide6 import *
-from pyqtgraph import *
+# from pyqtgraph import *
 from requests import *
-from netCDF4 import *
-from fpdf import *             # this is fpdf2
-from pdfrw import *
+# from netCDF4 import *
+# from fpdf import *             # this is fpdf2
+# from pdfrw import *
 from rapidfuzz import *
-from wavespectra import *
-from waveresponse import *
-from mafredo import *
+# from wavespectra import *
+# from waveresponse import *
+# from mafredo import *
 from nooverlap import *
 from PySide6QtAds import *
 from PIL import *
